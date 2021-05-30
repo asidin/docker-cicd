a26f15c124a063216d20eb254055bb57ab56b461
asidin <84897462+asidin@users.noreply.github.com>
2021-05-30 11:42:20 +0300

    Update nodejs_docker.groovy

--- job-dsl/nodejs_docker.groovy
+++ job-dsl/nodejs_docker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName(ac/dc)
+            repositoryName(asidin/dsl_nodejs)
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
